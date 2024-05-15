c8384601b4092ac750cfd1ccecefef5d28a93e29
ahmadfrnando <114858307+ahmadfrnando@users.noreply.github.com>
2024-05-15 12:08:22 +0700

    CHANGE DOWNLOAD BUTTON

--- components/Hero/hero.js
+++ components/Hero/hero.js
@@ -18,11 +18,11 @@ export default function HeroSection() {
             <p className="font-extrabold text-md text-orange">Hello 🖐️ Im Nando </p>
             <h1 className="blink text-4xl md:text-5xl font-black">Web Programer</h1>
             <p className="text-lg">Developing innovative technology solutions that combine programming expertise with a deep understanding of user needs</p>
-            <Link href="#">
+           
               <button onClick={() => download(fileUrl, filename)} className="px-8 py-4 mt-10 font-extrabold text-white transition duration-300 ease-in-out delay-150 bg-orange hover:bg-orange/80 hover:-translate-y-1 hover:scale-110 hover:bg-merah ">
                 Download CV
               </button>
-            </Link>
+           
           </div>
         </div>
       </div>
