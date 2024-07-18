0b771e62df20fdefeaff45ce4ebd96a001808d9a
ahmadfrnando <ahmadfernando88@gmail.com>
2024-07-18 07:15:04 +0700

    update cv

--- components/Hero/hero.js
+++ components/Hero/hero.js
@@ -20,7 +20,7 @@ export default function HeroSection() {
             <h1 className="blink text-4xl md:text-5xl font-black">Web Programer</h1>
             <p className="text-lg">Developing innovative technology solutions that combine programming expertise with a deep understanding of user needs</p>
             <div className='flex flex-row gap-10'>
-              <Link href="https://drive.google.com/file/d/1aG81ph10zCiClqhrM1a20_28V_xt-nHf/view?usp=sharing" target='_blank'>
+              <Link href="https://drive.google.com/file/d/1FD7Pd1R7W91Z-gxLIVOYjPJoGXZixC4O/view?usp=drive_link" target='_blank'>
                 <button onClick={() => download(fileUrl, filename)} className="px-8 py-4 mt-10 font-extrabold text-white transition duration-300 ease-in-out delay-150 bg-orange hover:bg-orange/80 hover:-translate-y-1 hover:scale-110 hover:bg-merah ">
                   Download CV
                 </button>
